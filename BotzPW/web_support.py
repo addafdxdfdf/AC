a4f62bfe5823f4fa4a99b7140dabb9b5b0a6c18d
KALI <FionaWilson520866@hotmail.com>
2024-12-06 11:13:44 +0530

    Update web_support.py

--- BotzPW/web_support.py
+++ BotzPW/web_support.py
@@ -1,12 +1,14 @@
 from aiohttp import web
 
+# Define routes
 BotzPW = web.RouteTableDef()
 
 @BotzPW.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("https://t.me/Techshyam_auto_caption_bot")
+    return web.json_response({"message": "https://t.me/Techshyam_auto_caption_bot"})
 
+# Create the web server
 async def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(BotzPW)
+    web_app = web.Application(client_max_size=30 * 1024 * 1024)  # 30 MB size limit
+    web_app.add_routes(BotzPW)  # Add routes to the app
     return web_app
